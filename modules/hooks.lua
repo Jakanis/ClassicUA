99ba69be9a310f93ba164553f0c0d08db89fa1a4
Yevhenii Kozii <varmid@gmail.com>
2022-07-26 02:11:41 +0300

    Fix modules load order. Cleanup hooks.lua

--- modules/hooks.lua
+++ modules/hooks.lua
@@ -1,11 +1,5 @@
 local _, addonTable = ...
 
-if not addonTable.M then
-    addonTable.M = {}
-end
-
-addonTable.M.zone_text = {}
-local M = addonTable.M.zone_text
 local tooltips = addonTable.M.tooltips
 local quests = addonTable.M.quests
 
