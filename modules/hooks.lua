5f5ab797c43ee24e35fd24774900cdfc865842af
Yevhenii Kozii <varmid@gmail.com>
2022-07-26 02:07:53 +0300

    Create quests module

--- modules/hooks.lua
+++ modules/hooks.lua
@@ -7,9 +7,10 @@ end
 addonTable.M.zone_text = {}
 local M = addonTable.M.zone_text
 local tooltips = addonTable.M.tooltips
+local quests = addonTable.M.quests
 
 
 tooltips.register_tooltip_hooks()
-
+quests.register_quests_hooks()
 
 
