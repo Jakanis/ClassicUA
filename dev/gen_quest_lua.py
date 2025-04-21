2e3ae2265474b44b7ad39935084e086b4f0e7d07
Yevhenii Kozii <varmid@gmail.com>
2025-04-21 14:08:52 +0300

    Generate DB with quest data for LLM usage

--- dev/gen_quest_lua.py
+++ dev/gen_quest_lua.py
@@ -13,7 +13,7 @@ def collect_quests():
         r for r in conn.execute('SELECT expansion, id FROM quests ORDER BY id')
     ]
 
-    quests = { e: { s: [] for s in utils.known_sides } for e in utils.known_expansions }
+    quests = { e: [] for e in utils.known_expansions }
     quests_cat_count = { e: {} for e in utils.known_expansions }
     quests_cat_total = { e: {} for e in utils.known_expansions }
     text_chars_used = {}
@@ -24,8 +24,8 @@ def collect_quests():
             issues.append(f'[!] Unexpected expansion "{expansion}" for #{id}.')
             continue
 
-        cat, side, title = conn.execute(
-            f'SELECT cat, side, title FROM quests WHERE expansion="{expansion}" AND id={id}'
+        cat, side, title, objective, description, progress, completion = conn.execute(
+            f'SELECT cat, side, title, objective, description, progress, completion FROM quests WHERE expansion="{expansion}" AND id={id}'
         ).fetchone()
 
         quests_folder_name = f'quests_{expansion}' if expansion != 'classic' else 'quests'
@@ -45,17 +45,24 @@ def collect_quests():
         quests_cat_total[expansion][cat] =\
             1 + quests_cat_total[expansion][cat] if cat in quests_cat_total[expansion] else 1
 
-        if not strings:
-            continue
+        # if not strings:
+        #     continue
 
         new_quest = {
             'id': id,
+            'expansion': expansion,
+            'title_en': title,
+            'objective_en': objective,
+            'description_en': description,
+            'progress_en': progress,
+            'completion_en': completion,
             'en': title,
-            'title': False,
-            'objective': False,
-            'description': False,
-            'progress': False,
-            'completion': False,
+            'title_uk': None,
+            'objective_uk': None,
+            'description_uk': None,
+            'progress_uk': None,
+            'completion_uk': None,
+            'cat': cat
         }
 
         new_quest_chars = {}
@@ -63,7 +70,7 @@ def collect_quests():
             if s.text:
                 string_attr = s.attrib['name'].lower()
                 string_text = utils.get_clean_text(s.text)
-                new_quest[string_attr] = string_text
+                new_quest[string_attr+'_uk'] = string_text
 
                 for c in string_text:
                     code = ord(c)
@@ -79,12 +86,13 @@ def collect_quests():
             if b1_code_count != b2_code_count:
                 issues.append(f'[?] Bracers "{b1}" and "{b2}" don\'t match\n\t- quest: {new_quest}')
 
-        if new_quest['title']:
-            print(f'{expansion} quest #{id} -> {new_quest["title"]}')
-            quests[expansion][side].append(new_quest)
+        if new_quest['title_uk']:
+            print(f'{expansion} quest #{id} -> {new_quest["title_uk"]}')
+            # quests[expansion][side].append(new_quest)
             quests_cat_count[expansion][cat] =\
                 1 + quests_cat_count[expansion][cat] if cat in quests_cat_count[expansion] else 1
 
+        quests[expansion].append(new_quest)
     return quests, quests_cat_count, quests_cat_total, text_chars_used, issues
 
 def print_report(quests, quests_cat_count, quests_cat_total, text_chars_used, issues):
@@ -123,20 +131,52 @@ def print_report(quests, quests_cat_count, quests_cat_total, text_chars_used, is
         for text in issues:
             print(text)
 
+def save_quests_to_cache_db(quests: dict[str, list[dict]]):
+    print('Saving quest data to cache DB')
+    conn = sqlite3.connect('cache/quests.db')
+    conn.execute('DROP TABLE IF EXISTS quests')
+    conn.execute('''CREATE TABLE quests (
+                        id INTEGER NOT NULL,
+                        expansion TEXT NOT NULL,
+                        title_en TEXT,
+                        objective_en TEXT,
+                        description_en TEXT,
+                        progress_en TEXT,
+                        completion_en TEXT,
+                        title_uk TEXT,
+                        objective_uk TEXT,
+                        description_uk TEXT,
+                        progress_uk TEXT,
+                        completion_uk TEXT,
+                        cat TEXT,
+	                    UNIQUE("id","expansion")
+                )''')
+
+    conn.commit()
+    with conn:
+        for quest_expansion, quests in quests.items():
+            for quest in quests:
+                conn.execute(f'''INSERT INTO quests(id, expansion, title_en, objective_en, description_en, progress_en, completion_en, title_uk, objective_uk, description_uk, progress_uk, completion_uk, cat)
+                                    VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)''',
+                                (quest["id"], quest["expansion"], quest["title_en"], quest["objective_en"],
+                                 quest["description_en"], quest["progress_en"], quest["completion_en"],
+                                 quest["title_uk"], quest["objective_uk"], quest["description_uk"], quest["progress_uk"], quest["completion_uk"], quest["cat"]))
+
 def main():
     sys.stdout.reconfigure(encoding='utf-8')
 
     quests, quests_cat_count, quests_cat_total, text_chars_used, issues = collect_quests()
 
-    for expansion in quests:
-        for side in quests[expansion]:
-            utils.write_lua_quest_file(
-                path    =f'../entries/{expansion}',
-                filename=f'quest_{side}',
-                var     =f'quest_{side}',
-                quests  =quests[expansion][side]
-            )
-
-    print_report(quests, quests_cat_count, quests_cat_total, text_chars_used, issues)
+    save_quests_to_cache_db(quests)
+    # for expansion in quests:
+    #     for side in quests[expansion]:
+    #         utils.write_lua_quest_file(
+    #             path    =f'translation_from_crowdin/entries/{expansion}',
+    #             filename=f'quest_{side}',
+    #             var     =f'quest_{side}',
+    #             quests  =quests[expansion][side]
+    #         )
+    #
+    # print_report(quests, quests_cat_count, quests_cat_total, text_chars_used, issues)
 
 main()
